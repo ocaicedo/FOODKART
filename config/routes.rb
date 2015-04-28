5acf389242aa94c585da0925613c8f2c88061de7
ocaicedo <caicedo.os@gmail.com>
2015-04-27 20:47:21 -0500

    se corrige orders_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
+# == Route Map
+#
+#        Prefix Verb   URI Pattern                    Controller#Action
+#          root GET    /                              products#index
+#      products GET    /products(.:format)            products#index
+#               POST   /products(.:format)            products#create
+#   new_product GET    /products/new(.:format)        products#new
+#  edit_product GET    /products/:id/edit(.:format)   products#edit
+#       product GET    /products/:id(.:format)        products#show
+#               PATCH  /products/:id(.:format)        products#update
+#               PUT    /products/:id(.:format)        products#update
+#               DELETE /products/:id(.:format)        products#destroy
+#    categories GET    /categories(.:format)          categories#index
+#               POST   /categories(.:format)          categories#create
+#  new_category GET    /categories/new(.:format)      categories#new
+# edit_category GET    /categories/:id/edit(.:format) categories#edit
+#      category GET    /categories/:id(.:format)      categories#show
+#               PATCH  /categories/:id(.:format)      categories#update
+#               PUT    /categories/:id(.:format)      categories#update
+#               DELETE /categories/:id(.:format)      categories#destroy
+#
+
 Rails.application.routes.draw do
   
   
