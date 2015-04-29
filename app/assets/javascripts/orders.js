b9fe930725dcba0547b292ca60684231d29df847
ocaicedo <caicedo.os@gmail.com>
2015-04-29 17:52:47 -0500

    se agrega jquery al form de la  vista orders

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,4 +1,13 @@
 
-$.ready(function() {
-		
+$(document).on('ready page:load', function(){
+$('.addField').on('click', function(){
+  var thisProduct = $(this).data("product-id");
+  var input = $('<input>');
+
+    input.attr('type', 'text');
+    input.attr('name', "order[order_products_attributes]["+thisProduct+"][product_id]");
+    input.attr('value', thisProduct);
+    input.appendTo($('form'));
+  });  
 })
+
